89978526e9b7473a6d739ec3ea1f4cd7385e33c9
komeil <komeilmohammadian1376.web@gmail.com>
2025-04-21 12:02:45 +0330

    fix(bug): swapElements.

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,7 @@ const kmList = lib;
 export default kmList;
 export * from './lib';
 
-
-let list = [11,22,33];
+let list = [11, 22, 33];
 
 list = kmList.swapElements(list, 0, 2);
 console.log(list); // [33, 22, 11]
