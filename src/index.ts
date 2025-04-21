736789eaa53bdb89da0c37c9f8f9ad1957d97e5e
komeil <komeilmohammadian1376.web@gmail.com>
2025-04-21 15:04:36 +0330

    fix(bug): insert before index.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,3 @@ import lib from './lib';
 const kmList = lib;
 export default kmList;
 export * from './lib';
-
-let list = [1, 2, 3, 4];
-
-list = kmList.insertElementBeforeThisIndex(list, 1, 3333);
